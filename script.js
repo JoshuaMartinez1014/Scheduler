85602e772b2c2d23e4d5db589433d0b3fb35af92
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-04-14 00:13:33 -0500

    final commit

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
 let hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-let hoursStandard = [
-  "9AM",
-  "10AM",
-  "11AM",
-  "12PM",
-  "1PM",
-  "2PM",
-  "3PM",
-  "4PM",
-  "5PM",
-];
+// prettier-ignore
+let hoursStandard = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM",];
 
 let timeRange;
 let today = dayjs().format("dddd, MMMM D YYYY");
@@ -59,32 +27,73 @@ for (let i = 0; i < hours.length; i++) {
   }
 
   $(".container-lg").append(`<div id="hour-${hours[i]}"></div>`);
-  $(`#hour-${hours[i]}`)
-    /* .attr("id", `hour-${hours[i]}`) */
-    .attr("class", `row time-block ${timeRange}`);
+  $(`#hour-${hours[i]}`).attr("class", `row time-block ${timeRange}`);
 
-  $(`#hour-${hours[i]}`)
-    .append(
-      `<div class="col-2 col-md-1 hour text-center py-3">${hoursStandard[i]} </div>`
-    )
-    .append(
-      `<textarea class="col-8 col-md-10 description" rows="3"> </textarea>`
-    ).append(`<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    <i class="fas fa-save" aria-hidden="true"></i>
-  </button>`);
+  const newDiv = $("<div>");
+  newDiv.addClass("col-2 col-md-1 hour text-center py-3");
+  newDiv.text(hoursStandard[i]);
+
+  const newTextArea = $("<textarea>");
+  newTextArea.addClass("col-8 col-md-10 description");
+  newTextArea.attr("rows", 3);
+
+  const newButton = $("<button>");
+  newButton.addClass("btn saveBtn col-2 col-md-1");
+  newButton.attr("aria-label", "save");
+  newButton.html(`<i class="fas fa-save" aria-hidden="true"></i>`);
+
+  $(`#hour-${hours[i]}`).append(newDiv).append(newTextArea).append(newButton);
 }
-
+let storage = [];
+let insertText;
 // button click saves textarea
-$(`button`).on("click", function (i) {
-  if (this) {
+$(`button`).on(
+  "click",
+  function (e) {
+    var hour = $(this).parent().attr("id");
+    /* if (this) { */
     console.log($(this).prev().val());
     text = $(this).prev().val();
-    localStorage.setItem(`#hour-${hours[i]}`, +"text");
+
+    console.log(e);
+    insertText = { id: hour, textContent: text };
+    pull();
   }
-});
+  /* } */
+);
 
+function pull() {
+  save = JSON.parse(localStorage.getItem("Data")) || [];
+  let overwritten = false;
+  let updatedSave = [];
 
-storage = {
-  id = #hour-1,
-  textvalue = decription
+  if (save.length == 0) {
+    updatedSave.push(insertText);
+  } else {
+    updatedSave = save.map((item) => {
+      if (item.id == insertText.id) {
+        overwritten = true;
+        return insertText;
+      } else {
+        return item;
+      }
+    });
+
+    if (!overwritten) {
+      updatedSave.push(insertText);
+    }
+  }
+  localStorage.setItem("Data", JSON.stringify(updatedSave));
 }
+
+function load() {
+  save = JSON.parse(localStorage.getItem("Data"));
+
+  save.forEach((item) => {
+    const id = item.id;
+    //  #hour-9 textarea
+    $(`#${id} textarea`).val(item.textContent);
+  });
+}
+
+load();
